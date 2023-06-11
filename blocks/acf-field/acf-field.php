4176fad440377f65ac586f2448b952ded1ff0ad0
Herb Miller <herb@bobbingwide.com>
2023-06-11 18:13:14 +0100

    Refactor to use acf_field_block_renderer class #1

--- blocks/acf-field/acf-field.php
+++ blocks/acf-field/acf-field.php
@@ -1,5 +1,6 @@
 <?php
 
+
 /**
  * acf-field block template.
  *
@@ -11,14 +12,23 @@
  *          or the post ID of the post hosting this block.
  * @param array $context The context provided to the block by the post or its parent block.
  */
-bw_trace2();
-bw_backtrace();
-bw_trace2( $block, "block", false);
-bw_trace2( $content, "content", false );
-bw_trace2( $context, "context", false );
+//bw_trace2();
+//bw_backtrace();
+//bw_trace2( $block, "block", false);
+//bw_trace2( $content, "content", false );
+//bw_trace2( $context, "context", false );
+//bw_trace2( $wp_block, "wp_block", false );
 
-
+/*
 if ( !function_exists( 'acf_field_block_display_field_block')) {
 	require_once __DIR__ . '/../../includes/acf-field.php';
 }
-acf_field_block_display_field_block( $block, $content, $is_preview, $post_id, $wp_block );
+acf_field_block_display_field_block( $block, $content, $context, $is_preview, $post_id, $wp_block );
+*/
+if ( !class_exists( 'acf_field_block_renderer')) {
+    require_once __DIR__ . '/../../includes/class-acf-field-block-renderer.php';
+}
+
+$renderer = new acf_field_block_renderer( $block, $content, $context, $is_preview, $post_id, $wp_block );
+$renderer->render();
+
